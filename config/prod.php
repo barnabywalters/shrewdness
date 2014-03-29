88efcbe1167274d125b86be4e3d41d77d94dc443
Barnaby Walters <barnaby@waterpigs.co.uk>
2014-03-29 18:43:07 +0000

    Added prod logger

--- config/prod.php
+++ config/prod.php
@@ -1,3 +1,9 @@
 <?php
 
+use Silex\Provider\MonologServiceProvider;
+
 require __DIR__.'/user.php';
+
+$app->register(new MonologServiceProvider(), array(
+	'monolog.logfile' => __DIR__.'/../logs/silex.log',
+));
