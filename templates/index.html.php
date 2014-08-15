9b55563d054ebc734017e690a47a1b9a20ab19c1
Barnaby Walters <barnaby@waterpigs.co.uk>
2014-08-15 19:20:36 +0000

    Fixed ?next redirect URL to be absolute

--- templates/index.html.php
+++ templates/index.html.php
@@ -1,4 +1,4 @@
 <form class="indieauth-form" action="/login/" method="post">
-	<input type="hidden" name="next" value="/" />
+	<input type="hidden" name="next" value="<?= $nextUrl ?>" />
 	<p><input class="indieauth-url" name="me" placeholder="e.g. yourdomain.com" required /> <button class="indieauth-submit" type="submit">Log In</button></p>
 </form>
